42660a71709a9a1f85f7abccc82949a6fe8a3542
Adma <ashadpm@gmail.com>
2018-01-15 19:34:27 -0500

    Update Employee.cs

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -52,12 +52,13 @@ namespace LinqProject.Model
             {
                 new Employee() { EmployeeID = 1, FirstName = "Adam", LastName = "Maharjan", Gender="Male", Salary = 4000, DepartmentID = 1 },
                 new Employee() { EmployeeID = 2, FirstName = "Catherine", LastName = "Maharjan", Gender="Female",Salary = 5500, DepartmentID = 1 },
-                new Employee(){ EmployeeID = 3, FirstName = "Elie", LastName = "Shrestha", Gender="Male", Salary = 54000, DepartmentID = 2 },
-                new Employee(){ EmployeeID = 4, FirstName = "Geny", LastName = "Shrestha", Gender="Female", Salary = 4000, DepartmentID = 2 },
-                new Employee(){ EmployeeID = 5, FirstName = "Enrique", LastName = "Aryal", Gender="Male", Salary = 2000, DepartmentID = 2 },
-                new Employee(){ EmployeeID = 6, FirstName = "Katrina", LastName = "Khanal", Gender="Female", Salary = 8000, DepartmentID = 3 },
-                new Employee(){ EmployeeID = 7, FirstName = "Geny", LastName = "Dongol", Gender="Female", Salary = 54000, DepartmentID = 3 },
-                new Employee(){ EmployeeID = 8, FirstName = "Oschar", LastName = "Gurung", Gender="Male", Salary = 56000, DepartmentID = 3 }
+                new Employee() { EmployeeID = 3, FirstName = "Elie", LastName = "Shrestha", Gender="Male", Salary = 54000, DepartmentID = 2 },
+                new Employee() { EmployeeID = 4, FirstName = "Geny", LastName = "Shrestha", Gender="Female", Salary = 4000, DepartmentID = 2 },
+                new Employee() { EmployeeID = 5, FirstName = "Enrique", LastName = "Aryal", Gender="Male", Salary = 2000, DepartmentID = 2 },
+                new Employee() { EmployeeID = 6, FirstName = "Katrina", LastName = "Khanal", Gender="Female", Salary = 8000, DepartmentID = 3 },
+                new Employee() { EmployeeID = 7, FirstName = "Geny", LastName = "Dongol", Gender="Female", Salary = 54000, DepartmentID = 3 },
+                new Employee() { EmployeeID = 8, FirstName = "Oschar", LastName = "Gurung", Gender="Male", Salary = 56000, DepartmentID = 3 },
+                new Employee() { EmployeeID = 9, FirstName = "Empty", LastName = "Name", Gender="Male", Salary = 55000 }
             };
         }
     }
